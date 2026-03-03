85aa35a1b8bcba11b011682072c3caa564c69a23
valls <olavalls@gmail.com>
2026-03-03 09:21:29 -0300

    test: add test to app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,14 @@
+from http import HTTPStatus  
+
 from fastapi.testclient import TestClient
 
-from fastapi_todo_list.app import app
+from fast_zero.app import app
 
-client = TestClient(app)
+
+def test_root_deve_retornar_ok_e_ola_mundo():  
+    client = TestClient(app)  
+
+    response = client.get('/')  
+
+    assert response.status_code == HTTPStatus.OK  
+    assert response.json() == {'message': 'Olá Mundo!'}
