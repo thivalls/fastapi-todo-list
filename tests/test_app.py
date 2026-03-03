03f5fda05a5a83fbf97b66f231d025ed6b82bf14
valls <olavalls@gmail.com>
2026-03-03 09:24:52 -0300

    chore: add gitignore

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,14 @@
-from http import HTTPStatus  
+from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+from fastapi_todo_list.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():  
-    client = TestClient(app)  
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
 
-    response = client.get('/')  
+    response = client.get('/')
 
-    assert response.status_code == HTTPStatus.OK  
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
